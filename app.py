4059742fd55a69086f3d937af66a389703a327a3
NateBigStone <n.gaut@bigstonemgmt.com>
2020-10-14 23:54:38 -0500

    simple form

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+
+@app.route('/item', methods=["GET", "POST"])
+def item_endpoint():
+    if request.method == 'POST':
+        req = request.form
+        food = req.get("food")
+        print(food)
+        return redirect(request.url)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
