ef846a1d224fe0c9ff06f3fb35c37ad106659fdf
NateBigStone <n.gaut@bigstonemgmt.com>
2020-10-21 23:03:19 -0500

    more envs

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ class Config(object):
     RECIPE_URL = os.getenv('RECIPE_URL')
     RECIPE_KEY = os.getenv('RECIPE_KEY')
     IMAGE_URL = os.getenv('IMAGE_KEY')
+    IMAGE_CONSTRUCT = os.getenv('IMAGE_CONSTRUCT')
     IMAGE_KEY = os.getenv('IMAGE_KEY')
