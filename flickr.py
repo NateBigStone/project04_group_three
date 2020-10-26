6451bcf3052672c805c1ab794fa1b65f7f41f125
xm6264jz <ahmed.abdinoor3@gmail.com>
2020-10-25 20:14:14 -0500

    used snake_case for variables

--- flickr.py
+++ flickr.py
@@ -15,7 +15,7 @@ def main():
 def getImage(item):
 # Docs at https://www.flickr.com/services/api/explore/flickr.photos.search
 # Search for pictures, modify to search for whatever tag you want
-    flickerSearchURL = 'https://api.flickr.com/services/rest/'
+    flicker_search_url = 'https://api.flickr.com/services/rest/'
     params = {
     'method': 'flickr.photos.search',
     'api_key': key,
@@ -25,28 +25,28 @@ def getImage(item):
     'sort': 'relevance',
     }
 # Search flickr for pictures
-    flickrResponse = requests.get(flickerSearchURL, params=params)
+    flickr_response = requests.get(flicker_search_url, params=params)
 # get json back
-    flickrResponseJson = flickrResponse.json()
+    flickr_response_json = flickr_response.json()
 
 # Get first json object ('photos') which contains another json object ('photo') which is an json array; each
 # element represents one photo. Take element 1
 
     try:
-        firstResponsePhoto = flickrResponseJson['photos']['photo'][0]
+        first_response_photo =  flickr_response_json['photos']['photo'][0]
 #confirm to get the exact json
-        logging.info(firstResponsePhoto)
+        logging.info( first_response_photo)
 # deal with this in the following way.
 # Extract the secret, server, id and farm; which you need to construct another URL to request a specific photo
 # https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-        secret = firstResponsePhoto['secret']
-        photo_id = firstResponsePhoto['id']
-        server = firstResponsePhoto['server']
-        farm = firstResponsePhoto['farm']
+        secret =  first_response_photo['secret']
+        photo_id =  first_response_photo['id']
+        server =  first_response_photo['server']
+        farm =  first_response_photo['farm']
     
-        fetchPhotoURL = f'https://farm{farm}.staticflickr.com/{server}/{photo_id}_{secret}_m.jpg'
-        photoResponse = requests.get(fetchPhotoURL)
-        return photoResponse
+        fetch_photo_url = f'https://farm{farm}.staticflickr.com/{server}/{photo_id}_{secret}_m.jpg'
+        photo_response = requests.get(fetch_photo_url)
+        return photo_response
     except Exception as e:
         logging.error(f'Something went wrong when calling the API: {e}')
 
