f07c44be59dcc998586bd12768162ef0aacd375d
NateBigStone <n.gaut@bigstonemgmt.com>
2020-11-01 11:36:11 -0600

    input validation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import config
 from model import Foods
+import logging
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ app.config.from_object(config.Config)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('search.html')
 
 
 @app.route('/item', methods=["GET", "POST"])
@@ -18,11 +19,31 @@ def item_endpoint():
     if request.method == 'POST':
         req = request.form
         food_post = req.get("food")
-        food_query.add_food(food_post)
+        food_valid = valid_food(food_post)
+        if not food_valid:
+            return render_template('search.html', error=True, error_text='Invalid input, please try again.')
+        else:
+            food_query.add_food(food_post)
         return redirect(request.url)
     else:
-        return render_template('search.html', food_response=[food_query.get_image(), food_query.get_recipe(),
-                                                             food_query.get_yelp()])
+        try:
+            image = food_query.get_image()
+            recipe = food_query.get_recipe()
+            yelp = food_query.get_yelp()
+            if image or recipe or yelp:
+                return render_template('search.html', food_response=[image, recipe, yelp])
+            else:
+                raise Exception
+        except Exception as e:
+            logging.warning(e)
+            return render_template('search.html', error=True, error_text='API error.')
+
+
+def valid_food(food_string):
+    if food_string.isalpha():
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
