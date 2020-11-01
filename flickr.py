3b5061d1f577051e4c500460706685d632a5ab48
whagestad <Slinky!12>
2020-11-01 13:59:49 -0600

    Edited flickr api key to reflect .env file key

--- flickr.py
+++ flickr.py
@@ -13,7 +13,7 @@ def main():
 def get_image(item):
     # Docs at https://www.flickr.com/services/api/explore/flickr.photos.search
     # Get flickr key
-    key = os.environ.get('flickr_key')
+    key = os.environ.get('IMAGE_KEY')
     # Search for pictures, modify to search for whatever tag you want
     flicker_search_url = 'https://api.flickr.com/services/rest/'
     params = {
