347d571c9bbcfd4b834285574604337bf2f5c1ec
whagestad <Slinky!12>
2020-11-01 15:40:58 -0600

    Adds explanation of json.loads, json.dump functions regarding recipe, minor cleanup of code

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 from food import get_dish_info
 from yelp import api_request
 from flickr import get_image
-from database import *
+from database import add_data, return_all, delete_record
 import json
+import ast
 
 
 class Foods:
@@ -14,6 +15,7 @@ class Foods:
 
     def add_food(self, food):
         self.food = food
+        add_data(self.restaurant, self.recipe, self.image)
 
     def get_food(self):
         if self.food:
@@ -26,6 +28,7 @@ class Foods:
     def get_recipe(self):
         self.recipe = get_dish_info(self.food) or None
         if self.recipe:
+            # converts recipe from dictionary to string
             recipe_string = json.dumps(self.recipe)
         else:
             recipe_string = None
@@ -36,8 +39,11 @@ class Foods:
         self.restaurant = api_request(self.food) or None
         return self.restaurant
 
-    def save_food(self):
-        add_data(self.restaurant, self.recipe, self.image)
+    def get_all_food(self):
+        # returns a list of "Foods" objects. For each entry, [0] = restaurant, [1] = recipe, [2] = flickr url
+        results = return_all()
+        return results
 
     def delete_food(self):
+        # removes the selected Food object from the database
         delete_record(self.restaurant, self.recipe, self.image)
