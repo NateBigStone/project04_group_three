de507d8225874ae06c361c83062d665ab9c50099
whagestad <Slinky!12>
2020-11-01 13:08:55 -0600

    Implements database to model.py class

--- database.py
+++ database.py
@@ -5,29 +5,17 @@ db = 'food_db.sqlite'
 
 def create_table():
     with sqlite3.connect(db) as conn:
-        conn.execute('CREATE TABLE IF NOT EXISTS records(restaurant TEXT, recipe TEXT, flickr TEXT, bookmarked INTEGER)')
+        conn.execute('CREATE TABLE IF NOT EXISTS records(restaurant TEXT, recipe TEXT, flickr TEXT)')
     conn.close()
 
 def add_data(yelp_response, edamam_response, flickr_response):
     bookmarked = 0
     try:
         with sqlite3.connect(db) as conn:
-            conn.execute('INSERT INTO records (restaurant, recipe, flickr, bookmarked) VALUES (?, ?, ?, ?)', (yelp_response, edamam_response, flickr_response, bookmarked))
+            conn.execute('INSERT INTO records (restaurant, recipe, flickr, bookmarked) VALUES (?, ?, ?)', (yelp_response, edamam_response, flickr_response))
     except Exception as e:
         logging.error('Error ' + e + ' insrting ' + yelp_response)
     conn.close()
-
-
-def toggle_bookmark(restaurant, recipe, flickr, bookmarked):
-    with sqlite3.connect(db) as conn:
-        if bookmarked == 0:
-            new_boomark = 1
-            conn.execute('UPDATE records SET bookmarked = ? WHERE (restaurant = ? AND recipe = ?)', (new_boomark, restaurant, recipe))
-        elif bookmarked == 1:
-            new_boomark = 0
-            conn.execute('UPDATE records SET bookmarked = ? WHERE (restaurant = ? AND recipe = ?)', (new_boomark, restaurant, recipe))
-    conn.close()
-
 
 def delete_record(restaurant, recipe, flickr):
     try:
@@ -42,11 +30,6 @@ def return_all():
         results = conn.execute('SELECT * FROM records')
     conn.close()
     return results
-
-def clear_unbookmarked():
-    with sqlite3.connect(db) as conn:
-        conn.execute('DELETE * FROM records WHERE bookmarked = ?', (0, ))
-    conn.close()
 
 
 
--- model.py
+++ model.py
@@ -1,11 +1,16 @@
 from food import get_dish_info
 from yelp import api_request
 from flickr import get_image
+from database import *
+import json
 
 
 class Foods:
     def __init__(self):
         self.food = ''
+        self.image = None
+        self.recipe = None
+        self.restaurant = None
 
     def add_food(self, food):
         self.food = food
@@ -15,10 +20,24 @@ class Foods:
             return self.food
 
     def get_image(self):
-        return get_image(self.food) or None
+        self.image = get_image(self.food) or None
+        return self.image
 
     def get_recipe(self):
-        return get_dish_info(self.food) or None
+        self.recipe = get_dish_info(self.food) or None
+        if self.recipe:
+            recipe_string = json.dumps(self.recipe)
+        else:
+            recipe_string = None
+        self.recipe = recipe_string
+        return self.recipe
 
     def get_yelp(self):
-        return api_request(self.food) or None
+        self.restuarant = api_request(self.food) or None
+        return self.restaurant
+
+    def save_food(self):
+        add_data(self.restaurant, self.recipe, self.image)
+
+    def delete_food(self):
+        delete_record(self.restaurant, self.recipe, self.image)
